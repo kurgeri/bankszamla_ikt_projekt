cce16006968cb6d4bac5b64e51bec8882c9621f9
kurgeri <b3kurger@vasvari.org>
2026-03-17 14:53:49 +0100

    Class

---
+++ banki_program_ikt/bankolas/bankolas/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankolas
+{
+    internal class Account
+    {
+        private string szamlaszam;
+        private string tulajdonos;
+        private decimal egyenleg;
+        private decimal hitelkeret;
+    }
+}
